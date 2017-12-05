2597a73c7757e02ad90a8de79e07e129566510df
Alvin.X.Zhang (g-mis.cncd02.Newegg) 42268 <az8g@buyabs.corp>
2017-12-05 16:13:40 +0800

    add router & method

---
+++ EasyREST/Core/ServiceMethod.cs
@@ -0,0 +1,22 @@
+﻿using EasyREST.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyREST.Core
+{
+    [AttributeUsage(AttributeTargets.Method,AllowMultiple = true,Inherited = false)]
+    class ServiceMethod: Attribute
+    {
+        private RequestMethods _Method;
+
+        private ServiceMethod(RequestMethods RequestMethods)
+        {
+            _Method = RequestMethods;
+        }
+
+        public RequestMethods Method { get => _Method; }
+    }
+}
---
+++ EasyREST/Core/ServiceRouter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyREST.Core
+{
+    [AttributeUsage(AttributeTargets.Class,AllowMultiple = true,Inherited = false)]
+    class ServiceRouter: Attribute
+    {
+        private string _RouterName;
+
+        public ServiceRouter(string RouterName)
+        {
+            _RouterName = RouterName;
+        }
+
+        public string RouterName { get => _RouterName; }
+    }
+}
